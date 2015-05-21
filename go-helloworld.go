77fdca44f1e4b928ba1cd9be36d3085e4f610eb3
junxu <junxu@marvell.com>
2015-05-21 17:15:01 +0800

    test from new desktop

--- go-helloworld.go
+++ go-helloworld.go
@@ -3,7 +3,24 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Printf("hello,world\n")
+	fsrc = open(filename_src)
+	defer fclose(fsrc)
+
+	if fdst = open(filename_dst) ? ->return;
+	defer fclose(fdst)
+
+	read fscr - > fdst.
+
+	defer fmt.Printf("hello,world\n")
+	defer fmt.Printf("this is the first hello\n")
+	
 
 }
 
+func fclose() {
+	if close ? panic xxx1;
+	else recover xxx2;
+}
+
+
+make slice of array;
