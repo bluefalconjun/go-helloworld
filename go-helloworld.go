62c99339590f9103eab97c04c5f9a3b74593f2c4
junxu <junxu@marvell.com>
2015-05-21 17:15:50 +0800

    Revert "test from new desktop"
    This reverts commit 77fdca44f1e4b928ba1cd9be36d3085e4f610eb3.

--- go-helloworld.go
+++ go-helloworld.go
@@ -3,24 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	fsrc = open(filename_src)
-	defer fclose(fsrc)
-
-	if fdst = open(filename_dst) ? ->return;
-	defer fclose(fdst)
-
-	read fscr - > fdst.
-
-	defer fmt.Printf("hello,world\n")
-	defer fmt.Printf("this is the first hello\n")
-	
+	fmt.Printf("hello,world\n")
 
 }
 
-func fclose() {
-	if close ? panic xxx1;
-	else recover xxx2;
-}
-
-
-make slice of array;
