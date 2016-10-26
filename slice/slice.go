22ed793b1364f6a99c972178615be3de04b14880
juxu <jun.xu@montage-tech.com>
2016-10-26 17:15:22 +0800

    update

---
+++ slice/slice.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	s := []int{2, 3, 5, 7, 11, 13}
+	fmt.Println("s ==", s)
+
+	for i := 0; i < len(s); i++ {
+		fmt.Printf("s[%d] == %d\n", i, s[i])
+	}
+
+    //s[lo:hi] means elements in s from lo to hi-1. excluding hi.
+    fmt.Println("s[1:4] ==", s[1:4])
+
+    //ignore lo then lo=0
+    fmt.Println("s[:3] ==", s[:3])
+
+    //ignore hi then hi=len(s)-1
+    fmt.Println("s[4:] ==", s[4:])
+}
