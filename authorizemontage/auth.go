ac32d01664b4db8cbde6959fa2989225343407e0
falcon.xu <jun.xu.falcon@gmail.com>
2018-08-30 15:38:29 +0800

    remove name

--- authorizemontage/auth.go
+++ authorizemontage/auth.go
@@ -9,24 +9,21 @@ import (
 
 func main() {
 
-	url := "https://shfwauth.montage-tech.com/web_auth/index.html"
+	url := "https://www.test.com/web_auth/index.html"
 
-	payload := strings.NewReader("edit_cookies=&admin_id=juxu&admin_pw=o5%26lejrffz")
+
+	payload := strings.NewReader("edit_cookies=&admin_id=username&admin_pw=userpwd")
 
 	req, _ := http.NewRequest("POST", url, payload)
 
 	req.Header.Add("Connection", "keep-alive")
 	req.Header.Add("Pragma", "no-cache")
 	req.Header.Add("Cache-Control", "no-cache")
-	req.Header.Add("Origin", "https://shfwauth.montage-tech.com")
 	req.Header.Add("Upgrade-Insecure-Requests", "1")
 	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
-	//req.Header.Add("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36")
 	req.Header.Add("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8")
-	req.Header.Add("Referer", "https://shfwauth.montage-tech.com/web_auth/index.html")
 	req.Header.Add("Accept-Encoding", "gzip, deflate, br")
 	req.Header.Add("Accept-Language", "zh-CN,zh;q=0.9,zh-TW;q=0.8,en-US;q=0.7,en;q=0.6")
-	req.Header.Add("Postman-Token", "83ed297f-6fb5-4789-a806-2ba45bbd0ef9")
 
 	res, _ := http.DefaultClient.Do(req)
 
