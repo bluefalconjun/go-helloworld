b892c0d34195e04207dc44dbbb554c1525a7b928
Xu Jun <jun.xu.falcon@gmail.com>
2019-12-17 09:33:54 +0800

    update shape interfaces

--- gostudy/Gowithtest/structs/structs.go
+++ gostudy/Gowithtest/structs/structs.go
@@ -1,14 +1,48 @@
 package structs
 
+import ("math")
+		
+
+
 type Rectangle struct {
-    Width float64
+    Width  float64
     Height float64
 }
 
-func Perimeter(rect Rectangle) float64 {
-    return 2* (rect.Width + rect.Height)
+type Circle struct {
+    Radius float64
 }
 
-func Area(rect Rectangle) float64 {
-    return rect.Width*rect.Height
+type Triangle struct {
+    Base   float64
+    Height float64
+}
+
+type Shape interface {
+    Perimeter() float64
+	Area() float64
+}
+
+func (c Circle) Perimeter() float64 {
+    return 2*math.Pi*c.Radius
+}
+
+func (r Rectangle) Perimeter() float64 {
+    return 2*(r.Width+r.Height)
+}
+
+func (t Triangle) Perimeter() float64 {
+    return 2*(t.Base+t.Height)
+}
+
+func (c Circle) Area() float64 {
+    return math.Pi*c.Radius*c.Radius
+}
+
+func (r Rectangle) Area() float64 {
+    return r.Width*r.Height
+}
+
+func (t Triangle) Area() float64 {
+    return t.Base*t.Height/2
 }
--- gostudy/Gowithtest/structs/structs_test.go
+++ gostudy/Gowithtest/structs/structs_test.go
@@ -3,23 +3,35 @@ package structs
 import "testing"
 
 
-func TestPerimeter(t *testing.T) {
-	rectangle := Rectangle{10.0, 10.0}
-    got := Perimeter(rectangle)
-    want := 40.0
-
-    if got != want {
-		//f means float64. %.2 means show two numbers after dot.
-        t.Errorf("got %.2f want %.2f", got, want)
-    }
-}
-
 func TestArea(t *testing.T) {
-	rectangle := Rectangle{12.0, 6.0}
-    got := Area(rectangle)
-    want := 72.0
 
-    if got != want {
-        t.Errorf("got %.2f want %.2f", got, want)
+	ShapeTests := []struct {
+        name    string
+        shape   Shape
+        hasPerimeter float64
+        hasArea float64
+    }{
+		//当测试用例不是一系列操作，而是事实的断言时，测试才清晰明了.
+        {name: "Rectangle", shape: Rectangle{Width: 12, Height: 6}, hasPerimeter:36, hasArea: 72.0},
+        {name: "Circle", shape: Circle{Radius: 10}, hasPerimeter:62.83185307179586231995926937088370, 
+                                                    hasArea: 314.1592653589793},
+        {name: "Triangle", shape: Triangle{Base: 12, Height: 6}, hasPerimeter:36, hasArea: 36.0},
+	}
+
+    for _, tt := range ShapeTests {
+        // using tt.name from the case to use it as the `t.Run` test name
+        t.Run(tt.name, func(t *testing.T) {
+            got := tt.shape.Area()
+            if got != tt.hasArea {
+                t.Errorf("%#v got %.2f want %.2f", tt.shape, got, tt.hasArea)
+            }
+        })
+
+        t.Run(tt.name, func(t *testing.T) {
+            got := tt.shape.Perimeter()
+            if got != tt.hasPerimeter {
+                t.Errorf("%#v got %.2f want %.2f", tt.shape, got, tt.hasPerimeter)
+            }
+        })
     }
 }
