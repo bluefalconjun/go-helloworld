9fd292bb6a2d458994728d373977e67e5346558b
Xu Jun <jun.xu.falcon@gmail.com>
2020-03-30 14:49:41 +0800

    update to goapp

--- gostudy/Gowithtest/reflect/reflect_test.go
+++ gostudy/Gowithtest/reflect/reflect_test.go
@@ -1,21 +1,117 @@
 package refelct
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
+
+/*
+什么是 interface
+
+由于函数使用已知的类型，例如 string，int 以及我们自己定义的类型，如 BankAccount，我们享受到了 Go 为我们提供的类型安全。
+
+这意味着我们可以免费获得一些文档，如果你试图向函数传递错误的类型，编译器就会报错。
+
+但是，你可能会遇到这样的情况，即你不知道要编写的函数参数在编译时是什么类型的。
+
+Go 允许我们使用类型 interface{} 来解决这个问题，你可以将其视为 任意 类型。
+
+所以 walk(x interface{}, fn func(string)) 的 x 参数可以接收任何的值。
+*/
+
+type Person struct {
+	Name    string
+	Profile Profile
+}
+
+type Profile struct {
+	Age  int
+	City string
+}
 
 func TestWalk(t *testing.T) {
 
-	expected := "Chris"
-	var got []string
+	cases := []struct {
+		Name          string
+		Input         interface{}
+		ExpectedCalls []string
+	}{
+		//以下列出了不同的测试结构体类型.
+		{
+			"Struct with one string field",
+			struct {
+				Name string
+			}{"Chris"},
+			[]string{"Chris"},
+		},
+		{
+			"Struct with two string fields",
+			struct {
+				Name string
+				City string
+			}{"Chris", "London"},
+			[]string{"Chris", "London"},
+		},
+		{
+			"Struct with non string field",
+			struct {
+				Name string
+				Age  int
+			}{"Chris", 33},
+			[]string{"Chris"},
+		},
+		{
+			"Nested fields",
+			Person{
+				"Chris",
+				Profile{33, "London"},
+			},
+			[]string{"Chris", "London"},
+		},
+		{
+			"Pointers to things",
+			&Person{
+				"Chris",
+				Profile{33, "London"},
+			},
+			[]string{"Chris", "London"},
+		},
+		{
+			"Slices",
+			[]Profile{
+				{33, "London"},
+				{34, "Reykjavík"},
+			},
+			[]string{"London", "Reykjavík"},
+		},
+		{
+			"Arrays",
+			[2]Profile{
+				{33, "London"},
+				{34, "Reykjavík"},
+			},
+			[]string{"London", "Reykjavík"},
+		},
+		{
+			"Maps",
+			map[string]string{
+				"Foo": "Bar",
+				"Baz": "Boz",
+			},
+			[]string{"Bar", "Boz"},
+		},
+	}
 
-	x := struct {
-		Name string
-	}{expected}
+	for _, test := range cases {
+		t.Run(test.Name, func(t *testing.T) {
+			var got []string
+			walk(test.Input, func(input string) {
+				got = append(got, input)
+			})
 
-	walk(x, func(input string) {
-		got = append(got, input)
-	})
-
-	if len(got) != 1 {
-		t.Errorf("wrong number of function calls, got %d want %d", len(got), 1)
+			if !reflect.DeepEqual(got, test.ExpectedCalls) {
+				t.Errorf("got %v, want %v", got, test.ExpectedCalls)
+			}
+		})
 	}
 }
